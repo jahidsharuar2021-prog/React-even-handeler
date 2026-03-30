c4d1091ed05833548f16bc615e73ff131deff28d
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-03-30 17:27:18 +0600

    React even handler

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+import './App.css'
+
+function App() {
+ function handleClick(){
+  alert('I am clicked.');
+ };
+
+ const handleClick3=()=>{
+  alert("clicked 3");
+ }
+
+ const handleAdd5 =(num)=>{
+  const newNum=num+5;
+  alert(newNum);
+ }
+
+  return (
+    <>
+     
+    <h1>Vite + React</h1>
+    <button onClick={handleClick}>Click me-1</button>
+    <button onClick={function handClick2(){
+      alert("clicked -2")
+    }}>Click me-2</button>
+    <button onClick={handleClick3}>Click me -3</button>
+    <button onClick={()=>alert('click 4')}>Click me -3</button>
+    <button onClick={()=>handleAdd5(7)}>Click me -4</button>
+
+    </>
+  )
+}
+
+export default App
